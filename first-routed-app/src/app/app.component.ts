57ae2bdfa0e168638105b3ddf219afdb42e3816e
ElPumpil <pumapillo.pena.giacomo.mattia@iisgalvanimi.edu.it>
2020-04-11 15:01:12 +0000

    finito punto 1

--- first-routed-app/src/app/app.component.ts
+++ first-routed-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from './spotify.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  query: string;
   title = 'first-routed-app';
-  obsTrack : Observable<Object>;
+  obsTrack: Observable<Object>;
+  results: any;
   // faccio iniettare lo spotify service e faccio una ricerca
-  constructor(public spotify : SpotifyService){
-    this.obsTrack = spotify.searchTrack("Pretty Woman");
-    this.obsTrack.subscribe((data)=>console.log(data)); //visualizzo la ricerca sulla console
+  constructor(public spotify: SpotifyService) {
+
+  }
+
+  submit(query:HTMLInputElement): void {
+    if (!query.value) {
+      return;
+    }
+    this.query = query.value;
+    this.obsTrack = this.spotify.searchTrack(this.query);
+    this.obsTrack.subscribe((data) => this.results = data);
   }
 }
